c6e047e40d2a783e44260394ecd1c4973508d301
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-07 13:13:18 +0530

    feat: configure EF Core DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceProcessingService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceProcessingService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Invoice> Invoices { get; set; }
+    }
+}
