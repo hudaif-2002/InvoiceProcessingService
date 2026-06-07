f395827efea50a4e9521ea65926ddd84efe196a1
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-07 13:49:14 +0530

    feat :Added Service interface

---
+++ Services/IInvoiceService.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceProcessingService.DTOs;
+using InvoiceProcessingService.Models;
+
+namespace InvoiceProcessingService.Services
+{
+    public interface IInvoiceService
+    {
+            Task<Invoice> CreateInvoice(CreateInvoiceDto dto);
+            Task<List<Invoice>> GetAllInvoiceAsync();
+            Task<Invoice> GetById(int id);
+    }
+}
