5ee93045f63cfed083551d09035bca922a711406
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-07 13:48:44 +0530

    feat :Added controller

---
+++ Controllers/InvoicesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InvoiceProcessingService.DTOs;
+using InvoiceProcessingService.Services;
+
+namespace InvoiceProcessingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvoicesController : Controller
+    {
+        private readonly IInvoiceService _invoiceService;
+
+        public InvoicesController(IInvoiceService invoiceService) =>
+            _invoiceService = invoiceService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+            Ok(await _invoiceService.GetAllInvoiceAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var invoice = await _invoiceService.GetById(id);
+            if (invoice != null)
+                return Ok(invoice);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateInvoiceDto dto)
+        {
+            try
+            {
+                var invoice = await _invoiceService.CreateInvoice(dto);
+                //return CreatedAtAction(nameof(GetById), new { id = invoice.Id }, invoice);
+                return Ok($"invoice created: {invoice}");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
