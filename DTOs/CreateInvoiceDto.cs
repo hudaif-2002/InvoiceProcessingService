91937d62cded8febbbc4c8253c5fd68c4449df53
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-07 10:57:40 +0530

    feat : Added Invoice Dto so that we can restrict client editing Id and CreatedAt

---
+++ DTOs/CreateInvoiceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InvoiceProcessingService.DTOs
+{
+    public class CreateInvoiceDto
+    {
+        public string CustomerName { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
