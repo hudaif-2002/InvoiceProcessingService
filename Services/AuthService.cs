10adb4c2607f10c560313a2d46670654e97b6bd7
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-09 21:18:40 +0530

    feat : Added Auth Service

---
+++ Services/AuthService.cs
@@ -0,0 +1,39 @@
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace InvoiceProcessingService.Services;
+public class  AuthService :IAuthService
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string? Authenticate(string username, string password)
+    {
+        if (username != "admin" && password != "password")
+            return null;
+     
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(1),
+            signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ Services/IAuthService.cs
@@ -0,0 +1,5 @@
+namespace InvoiceProcessingService.Services;
+public interface IAuthService
+{
+	string? Authenticate(string username, string password);
+}
