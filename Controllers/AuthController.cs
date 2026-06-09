b36b17a6a4d3063dd8d81e27252923fef3005817
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-09 21:18:22 +0530

    feat : Added Auth Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using InvoiceProcessingService.Services;
+using InvoiceProcessingService.DTOs;
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceProcessingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController :ControllerBase
+    {
+
+            private readonly IAuthService _authService;
+            public AuthController(IAuthService authService)
+            {
+                _authService = authService;
+            }
+
+            [HttpPost("login")]
+            public IActionResult Login([FromBody] LoginDto request)
+            {
+                var token = _authService.Authenticate(request.Username, request.Password);
+                if (token == null)
+                {
+                    return Unauthorized();
+                }
+                return Ok(new { Token = token });
+            }
+
+
+        }
+
+    
+}
