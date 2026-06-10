fac0a6d2c8b1f8a73ebb9a852fcdfdf2e4640f0a
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-10 23:24:56 +0530

    rafactor : configs

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,6 @@
 ﻿import axios from "axios";
-const API_URL = "https://localhost:7120/api/auth/";
+const API_URL =
+    `${import.meta.env.VITE_API_BASE_URL}/auth`;
 
 const register = async (userData) => {
     return await axios.post(API_URL + "register", userData);
