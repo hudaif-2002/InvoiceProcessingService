464070a77f1b736f5ca087d49ae24f0c8089b40b
hudaif-2002 <hudaifahamed08@gmail.com>
2026-06-10 23:30:25 +0530

    refactor : fixed deployemnet build issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login"; 
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard"
 import InvoiceList from "./pages/InvoiceList";
 import CreateInvoice from "./pages/CreateInvoice";
